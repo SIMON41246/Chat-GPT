a1b6554d621f64fae42824c1d8ba439a834f1f27
Mohamed Ezriouil <mohammedezriouil@gmail.com>
2023-04-22 10:24:28 +0000

    Chat-GPT APP using API openai.com

--- app/src/main/java/com/example/chatgptapp/APIKEY.kt
+++ app/src/main/java/com/example/chatgptapp/APIKEY.kt
@@ -2,5 +2,5 @@ package com.example.chatgptapp
 
 object APIKEY {
     const val url = "https://api.openai.com/"
-    const val theapikey="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"
+    const val theapikey = "YOUR_API_KEY"
 }
